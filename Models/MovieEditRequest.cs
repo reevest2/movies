52ae346a9d6f14b2c913a8363abe5585ed84481b
reevest2 <reevest2@gmail.com>
2021-10-02 21:30:37 -0400

    made edit buttons dry

---
+++ Models/MovieEditRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace movies.Models
+{
+    public class MovieEditRequest
+    {
+        
+        //MongoDB has this set to unique
+        public string Name { get; set; }
+
+        [Range(0, 10)]
+        public int Score { get; set; }
+
+        public string Review { get; set; }
+
+        public bool MustWatch { get; set; }
+    }
+}
